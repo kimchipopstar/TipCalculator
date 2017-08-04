993d8ed232162ca6dfa00d22a419d28c2a147a87
J.K <K_940201@hotmail.com>
2017-08-04 11:22:37 -0700

    Initial Commit

---
+++ TipCalculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TipCalculator
+//
+//  Created by Jaewon Kim on 2017-08-04.
+//  Copyright © 2017 Jaewon Kim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TipCalculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TipCalculator
+//
+//  Created by Jaewon Kim on 2017-08-04.
+//  Copyright © 2017 Jaewon Kim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
